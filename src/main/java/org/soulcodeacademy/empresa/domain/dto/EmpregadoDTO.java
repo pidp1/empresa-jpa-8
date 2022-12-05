5a53f5a51b4798f8ccfc31aab56441e3b6b8f38c
flafonceca <flaziff@gmail.com>
2022-12-05 13:08:39 -0300

    empregado realizado

--- src/main/java/org/soulcodeacademy/empresa/domain/dto/EmpregadoDTO.java
+++ src/main/java/org/soulcodeacademy/empresa/domain/dto/EmpregadoDTO.java
@@ -1,12 +1,26 @@
 package org.soulcodeacademy.empresa.domain.dto;
 
+import org.soulcodeacademy.empresa.domain.Endereco;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 public class EmpregadoDTO {
-    @NotNull(message = "idEmpregado é obrigatório")
+
+    @NotBlank(message = "Este campo é obrigatorio")
     private Integer idEmpregado;
+
+    @NotBlank(message = "Este campo é obrigatorio")
     private String nome;
+
+    @NotBlank(message = "Este campo é obrigatorio")
     private String email;
+
+    @NotNull(message = "Este campo é obrigatorio")
+    @Min(value = 500, message = "Campo salário inválido")
+    @Max(value = 100000, message = "Campo salário inválido")
     private Double salario;
 
     public Integer getIdEmpregado() {
